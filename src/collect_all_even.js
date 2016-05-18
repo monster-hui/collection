c3bd6df4c8d8faa6d2fde352d7cb8952d65d81ee
monster-hui <373000945@qq.com>
2016-05-18 19:24:11 +0800

    add  function
    补充函数

--- src/collect_all_even.js
+++ src/collect_all_even.js
@@ -1,7 +1,14 @@
 'use strict';
 
 function collect_all_even(collection) {
-  //在这里写入代码
+  var evens=[]
+  var patt = /\d*[0,2,4,6,8]-/g;
+  result=collection.join('-').match(patt);
+  alert(result)
+  for(var i=0;i<result.length;i++){
+  	evens.push(parseInt(result[i]));
+  	}
+  return evens;
 }
 
 module.exports = collect_all_even;
